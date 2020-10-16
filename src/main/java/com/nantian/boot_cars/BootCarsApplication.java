c6e0fc6c77c961d0624a20fdd1b3bc4fce9bcaaa
jiajingfei <15131066253@163.com>
2020-10-16 11:37:28 +0800

    Initial commit

---
+++ src/main/java/com/nantian/boot_cars/BootCarsApplication.java
@@ -0,0 +1,13 @@
+package com.nantian.boot_cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootCarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nantian/boot_cars/BootCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nantian.boot_cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
