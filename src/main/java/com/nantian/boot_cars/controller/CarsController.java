6abd8497e1dbef424a3e68f6f0cd12dd66f18164
jiajingfei <15131066253@163.com>
2020-10-16 11:48:36 +0800

    add CarsController

---
+++ src/main/java/com/nantian/boot_cars/controller/CarsController.java
@@ -0,0 +1,12 @@
+package com.nantian.boot_cars.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+public class CarsController {
+
+    @RequestMapping("/getCars")
+    public void getCars(){
+
+    }
+}
